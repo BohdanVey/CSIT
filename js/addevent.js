4246792e45ccd4a6ab36b1b5857504b474548499
BohdanVey <bogdan.vey2002@gmail.com>
2019-03-10 14:22:50 +0200

    image

--- js/addevent.js
+++ js/addevent.js
@@ -1,12 +1,27 @@
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
+
 function addevent() {
-	let image=document.getElementById("image").files.name;
-	console.log(image);
-    let name = document.getElementById("name").value;
-	let date = document.getElementById("date").value;
-	let hourbegin = document.getElementById("hourbegin").value;
-	let hourend=document.getElementById("hourend").value;
-	let place=document.getElementById("place").value;
-	let description=document.getElementById("description").value;
-	postevent(name,date,hourbegin,hourend,place,description,image);
-	alert("Ready");
+	let file=document.getElementById("image").files[0];
+	getBase64(file).then(
+	  image => {
+		    let name = document.getElementById("name").value;
+			let date = document.getElementById("date").value;
+			let hourbegin = document.getElementById("hourbegin").value;
+			let hourend=document.getElementById("hourend").value;
+			let place=document.getElementById("place").value;
+			let description=document.getElementById("description").value;
+			postevent(name,date,hourbegin,hourend,place,description,image);
+			alert("Ready");
+	  }
+	);
 };	
+
+
