6be1e5937365018d822a972fcbeb3d208a9f0cf5
BohdanVey <bogdan.vey2002@gmail.com>
2019-03-10 13:31:26 +0200

    addevent

--- js/addevent.js
+++ js/addevent.js
@@ -1,11 +1,11 @@
 function addevent() {
-	image=document.getElementById("image").value;
+	let image=document.getElementById("image").value;
     let name = document.getElementById("name").value;
 	let date = document.getElementById("date").value;
 	let hourbegin = document.getElementById("hourbegin").value;
 	let hourend=document.getElementById("hourend").value;
 	let place=document.getElementById("place").value;
 	let description=document.getElementById("description").value;
-	postevent(name,date,hoursBegin,hoursEnd,place,description,image);
+	postevent(name,date,hoursbegin,hoursend,place,description,image);
 	alert("Ready");
 };	
